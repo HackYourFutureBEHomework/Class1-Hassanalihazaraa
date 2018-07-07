37c1dd8f39f0c74230c747460cba4d17749a0ae7
Hassan <39343120+Hassanalihazaraa@users.noreply.github.com>
2018-07-07 18:21:06 +0200

    Update scrape.js

--- scrape.js
+++ scrape.js
@@ -1,5 +1,26 @@
 // Your custom scraping script.
 
+
+document.addEventListener("keypress", function() {
+    let table = document.querySelector("table.cast_list");
+    let rows = table.querySelectorAll("tr.odd, tr.even");
+    console.clear()
+    for (let i = 0; i < rows.length; i++) {
+        const row = rows[i];
+        let actor = row.querySelector("[itemprop=actor] a span").innerHTML;
+        let characterTag = row.querySelector("td.character");
+    let character;
+    let a = characterTag.querySelector("a");
+    if (a != null) {
+        character = a.innerHTML;
+    } else {
+        character = characterTag.innerHTML;
+    }
+    console.log("Actor: " + actor.trim() + ", Character: " + character.trim());
+}
+});
+
+
 // Look at https://github.com/HackYourFutureBelgium/JavaScript2/tree/master/Projects/web-scraper
 // for the version we showed in class.
 
