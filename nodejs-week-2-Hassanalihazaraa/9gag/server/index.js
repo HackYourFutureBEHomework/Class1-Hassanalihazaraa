b12b159fb285af45e3b63543657518ca07facd1a
Hassan <39343120+Hassanalihazaraa@users.noreply.github.com>
2018-09-01 23:27:02 +0200

    Update index.js

--- nodejs-week-2-Hassanalihazaraa/9gag/server/index.js
+++ nodejs-week-2-Hassanalihazaraa/9gag/server/index.js
@@ -38,6 +38,92 @@ server.post("/update/joke/upvote", (request, response) => {
                 }
                 response.json({status: "succes",message: "joke upvoted"})
             })
+               }
+    } 
+    response.send("update joke will come here");
+})
+function showError(error, response){
+    console.log(error);
+    response.json({status: "error", message: "something went wrong"});
+}
+
+
+
+server.post("/post/joke",(request,response)=>{
+    const {body}= request;
+    if(body){
+        const{title,file} = body;
+        if(file){
+            const {base64}=file;
+            const fileName =uuidv4();
+            fs.writeFile(`./public/images/${fileName}.jpeg`,base64,'base64',(error)=>{
+                if(error){
+                    console.log(error);
+                }
+            })
+            const sql = "INSERT into joke set ?";
+            const values ={
+                image_location:`http://10.20.0.48:8000/images/${fileName}.jpeg`,
+                title
+            }
+        
+            connection.query(sql,values,(error,result)=>{
+            if(error) showError(error,response);
+            response.json({status: "succes",  message: "joke uploaded"})
+            })
+        }   
+    }
+})
+
+
+
+
+server.get("/get/joke/:id",(request,response)=>{
+    const {id}= request.params;
+    const values=[id];
+    connection.query(`select * from joke where id =? `,values,(error,results)=>{
+    if(error){
+        showError(error,response);
+    }
+    response.json(results[0]);
+    })
+});
+
+
+
+
+server.get("/get/comments/:jokeId",(request,response)=>{
+    const {jokeId} = request.params;
+    const values = [jokeId];
+    connection.query(`select * from comment where joke_id=?`,values,(error,results)=>{
+        if(error)
+        showError(error.response);
+        response.json(results);
+    });
+});
+
+
+
+
+server.post("/post/comment",(request,response)=>{
+    const {body} = request;
+    if(body){
+        const{text,username,joke_id} = body;
+        const sql ="insert into comment set ?";
+        const values = {
+            text,
+            username,
+            joke_id
+        }
+        connection.query(sql,values,(error,result)=>{
+            if(error){
+                showError(error,response);
+            }
+            response.json({status: "succes",  message: "comment posted"})
+        });
+    }
+});
+         
         }
     } 
     response.send("update joke will come here");
