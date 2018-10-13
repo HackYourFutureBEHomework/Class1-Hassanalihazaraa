af01b3d77b59fd53c3746bdf81580bb454fd8f32
Hassan <39343120+Hassanalihazaraa@users.noreply.github.com>
2018-10-13 21:59:57 +0200

    Add files via upload

---
+++ react week 2/src/App.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import './App.css';
+import Header from './components/header';
+import TodoInput from './components/todoInput';
+import TodoItem from './components/todoItem';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      todos: [
+        {id: 0, text: "Get-out-of-bed, 2017-09-11"},
+        {id: 1, text: "Brush teeth, 2017-09-10"},
+        {id: 2, text: "Eat breakfast, 2017-09-09"}
+      ],
+      nextId: 3
+    };
+
+    this.addTodo = this.addTodo.bind(this);
+    this.removeTodo = this.removeTodo.bind(this);
+  }
+
+  addTodo(todoText) {
+    let todos = this.state.todos.slice();
+    todos.push({id: this.state.nextId, text: todoText});
+    this.setState({
+      todos: todos,
+      nextId: ++this.state.nextId
+    });
+  }
+
+  removeTodo(id) {
+    this.setState({
+        todos: this.state.todos.filter((todo, index) => todo.id !== id)
+      });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div className="todo-wrapper">
+          <Header />
+          <TodoInput todoText="" addTodo={this.addTodo} />
+          <ul>
+            {
+              this.state.todos.map((todo) => {
+                return <TodoItem todo={todo} key={todo.id} id={todo.id} removeTodo={this.removeTodo}/>
+              })
+            }
+          </ul>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
